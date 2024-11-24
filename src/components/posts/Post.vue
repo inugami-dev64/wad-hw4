6f47352ebea1819334437acbbc86fa8f191effa1
Pinsway <ruud.tammel@gmail.com>
2024-11-24 20:17:59 +0200

    Like system + reset likes

--- src/components/posts/Post.vue
+++ src/components/posts/Post.vue
@@ -16,39 +16,60 @@
 
 <template>
     <div class="post-container" :id="`post-${postContent.id}`">
-        <header>
-            <a href="/WAD-HW2/">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" class="bi bi-person-circle profile" viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-                </svg>
-            </a>
-            <time>{{ postContent.created_at }}</time>
-        </header>
-        <div class="post-content">
-            <div class="post-img-preview" v-if="'image_path' in postContent">
-                <a :href="imageUrl" target="_blank">
-                    <img :src="thumbnailUrl" alt="Image"/>
-                </a>
-            </div>
-            <div class="post-message">
-                <p>{{ postContent.content }}</p>
-            </div>
+      <header>
+        <a href="/WAD-HW2/">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" class="bi bi-person-circle profile" viewBox="0 0 16 16">
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+          </svg>
+        </a>
+        <time>{{ postContent.created_at }}</time>
+      </header>
+      <div class="post-content">
+        <div class="post-img-preview" v-if="'image_path' in postContent">
+          <a :href="imageUrl" target="_blank">
+            <img :src="thumbnailUrl" alt="Image"/>
+          </a>
         </div>
-        <div class="post-stats">
-            <button class="btn-transparent">
-                <img :src="thumbsupUrl" alt="Like" height="32" width="32">
-            </button>
+        <div class="post-message">
+          <p>{{ postContent.content }}</p>
         </div>
+      </div>
+      <div class="post-stats">
+        <button class="btn-transparent" @click="likePost">
+          <img :src="thumbsupUrl" alt="Like" height="32" width="32">
+        </button>
+        <span>{{ postContent.likes }} Likes</span>
+      </div>
     </div>
-</template>
-
-<script>
-    export default {
-        name: "PostContainer",
-        props: ["postContent"]
+  </template>
+  
+  <script>
+  export default {
+    name: "PostContainer",
+    props: ["postContent"],
+    data() {
+      return {
+        imageUrl: "",
+        thumbnailUrl: "",
+        thumbsupUrl: require(`@/assets/user-content/img/thumbs-up.svg`)
+      };
+    },
+    methods: {
+      likePost() {
+        this.$emit('like');
+      }
+    },
+    watch: {
+      'postContent.image_path'(newValue) {
+        if (newValue) {
+          this.imageUrl = require(`@/assets/user-content/img/${newValue}`);
+          this.thumbnailUrl = require(`@/assets/user-content/img/${newValue.replace(/^([A-Za-z0-9_\-]+)\.(jpg|png|gif)$/, 'thumbnails/$1_s\.$2')}`);
+        }
+      }
     }
-</script>
+  };
+  </script>
 
 <style scoped>
     /* Post container styling */
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,224 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    posts: [
+      {
+        id: 1,
+        created_at: "2024-11-03T17:02:30+0200",
+        author: "Ruud",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        likes: 10,
+        image_path: "anime-irl.jpg",
+        comments: [
+            {
+                author: "John Pork",
+                text: "I like this post",
+                created_at: "2024-11-03T17:02:30+0200"
+            },
+            {
+                author: "Mihkel Jaakson",
+                text: "Hehe",
+                created_at: "2024-11-03T17:02:30+0200"
+            }
+        ]
+      },
+      {
+          id: 2,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "Tartu is an interesting place",
+          likes: 13,
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "I agree!",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "True and real!",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+
+      },
+      {
+          id: 3,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "See pilt läheb väga kõvasti!",
+          likes: 120,
+          image_path: "kuva.jpg",
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "Awesome picture!",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      },
+      {
+          id: 4,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "Does anybody know where the lecture takes place?",
+          likes: 19,
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "Nope",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "Let me know aswell",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      },
+      {
+          id: 5,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "The joke is that the place is called Sneed's Feed & Seed which is clever in itself and quite funny to those with a mature sense of humour but what's really just hilarious about it is that if you look closely at the front of this store, Sneed's Feed & Seed, you can see a line that reads Formerly Chuck's. Now, this might go over the average viewer's head as this, THIS, is peak comedy. I doubt anything will ever be as funny as the joke about Sneed's Feed & Seed. Are you ready for this one? So, like I said, the place is called Sneed's Feed & Seed and this sign says Formerly Chuck's, which means that when Chuck owned the place, well, I don't have to tell you...",
+          likes: 34,
+          image_path: "sneed.png",
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "I don't find it funny",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "Good joke!",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      },
+      {
+          id: 6,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "Example post",
+          likes: 34,
+          image_path: "kuva.jpg",
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "Testing Testing 123",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "Smooth Criminal",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      },
+      {
+          id: 7,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "TestPost",
+          likes: 34,
+          image_path: "kuva.jpg",
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "Testing Testing 123",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "Smooth Criminal",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      },
+      {
+          id: 8,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "What am I doing?",
+          likes: 34,
+          image_path: "kuva.jpg",
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "Testing Testing 123",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "Smooth Criminal",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      },
+      {
+          id: 9,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "How is it even possible?",
+          likes: 34,
+          image_path: "kuva.jpg",
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "Testing Testing 123",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "Smooth Criminal",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      },
+      {
+          id: 10,
+          created_at: "2024-11-03T17:02:30+0200",
+          author: "Ruud",
+          content: "Please try to understand this code",
+          likes: 15,
+          image_path: "kuva.jpg",
+          comments: [
+              {
+                  author: "John Pork",
+                  text: "I will try",
+                  created_at: "2024-11-03T17:02:30+0200"
+              },
+              {
+                  author: "Mihkel Jaakson",
+                  text: "Moonwalk",
+                  created_at: "2024-11-03T17:02:30+0200"
+              }
+          ]
+      }
+    ]
   },
   getters: {
+    posts: (state) => state.posts,
   },
   mutations: {
+    increaseLike(state, postId) {
+      const post = state.posts.find(post => post.id === postId);
+      if (post) post.likes++;
+    },
+    resetLikes(state) {
+      state.posts.forEach(post => {
+        post.likes = 0;
+      });
+    }
   },
   actions: {
+    increaseLike({ commit }, postId) {
+      commit('increaseLike', postId);
+    },
+    resetLikes({ commit }) {
+      commit('resetLikes');
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
