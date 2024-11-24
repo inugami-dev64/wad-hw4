63db512ba6528c061f3c40c6a21dd480e9d1e1f9
inugami-dev64 <karl@karlott.net>
2024-11-24 23:05:10 +0200

    Create success/failure banner components

---
+++ src/components/messages/FailureBanner.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="message failure" v-html="content"></div>
+</template>
+
+<script>
+    export default {
+        name: "FailureBanner",
+        props: ["content"]
+    }
+</script>
+
+<!-- Unscoped styling for generally for messages -->
+<style>
+    .message { 
+        padding: 1rem;
+        font-family: "Roboto", sans-serif;
+        font-size: var(--fs-base);
+        color: var(--fg-primary);
+        border-radius: 0.5rem;
+        width: 100%;
+        box-sizing: border-box;
+    }
+
+    .failure {
+        border: 1px solid var(--border-failure-banner);
+        background-color: var(--bg-failure-banner);
+    }
+</style>
---
+++ src/components/messages/SuccessBanner.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="message success" v-html="content"></div>
+</template>
+
+<script>
+    export default {
+        name: "FailureBanner",
+        props: ["content"]
+    }
+</script>
+
+<!-- Unscoped styling for generally for messages -->
+<style>
+    .message { 
+        padding: 1rem;
+        font-family: "Roboto", sans-serif;
+        font-size: var(--fs-base);
+        color: var(--fg-primary);
+        border-radius: 0.5rem;
+        width: 100%;
+        box-sizing: border-box;
+    }
+
+    .success {
+        border: 1px solid var(--border-success-banner);
+        background-color: var(--bg-success-banner);
+    }
+</style>
