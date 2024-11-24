5c70989589c39498dc535931fbfaf04abde41056
inugami-dev64 <karmen@nyaa.ee>
2024-11-24 14:36:25 +0200

    Make use of new Header and Footer components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,101 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <header-component/>
   <router-view/>
+  <footer-component/>
 </template>
 
-<style>
+<script setup>
+import HeaderComponent from './components/Header.vue';
+import FooterComponent from './components/Footer.vue';
+</script>
+
+<style global>
+/* We will use Roboto font, because I think it looks quite nice :) */
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+/* Let's define a bunch of universal properties */
+:root {
+    /* Background colors */
+    --bg-primary: #1f1f1f;
+    --bg-secondary: #38383d;
+    --bg-tertiary: #525257;
+    --bg-post-1: #110a53;
+
+    /* Foreground colors */
+    --fg-primary: #fff;
+    --fg-secondary: #ddd;
+    --fg-tertiary: #aaa;
+
+    --fg-light-red: rgb(244, 94, 94);
+
+    /* Font sizes */
+    --fs-base: 1rem;
+    --fs-h1: calc(var(--fs-base) * 2);
+    --fs-h2: calc(var(--fs-base) * 1.75);
+    --fs-h3: calc(var(--fs-base) * 1.5);
+    --fs-h4: calc(var(--fs-base) * 1.25);
+    --fs-h5: calc(var(--fs-base) * 1);
+    --fs-nav: calc(var(--fs-base) * 1.15);
+
+    /* Hovering transition */
+    --tr-hover: all 0.2s ease-in;
+}
+
+/* Remove default margins and paddings from the body element */
+body {
+    margin: 0;
+    padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  /* App divisor contains three elements: nav, main and footer.
+       For alignment reasons let's use grid for now to make footer stick to the bottom */
+  display: grid;
+  height: 100vh;
+  grid-template-rows: auto 1fr auto;
+  width: 100%;
+  background-color: var(--bg-primary);
 }
 
-nav {
-  padding: 30px;
+/* Text element styles */
+span, h1, h2, h3, h4, h5, p, a, b, i, li, label, time {
+    font-family: "Roboto", sans-serif;
+    font-size: var(--fs-base);
+    color: var(--fg-primary);
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+time {
+    font-weight: 900;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+/** Remove bullets from <li> elements **/
+li { list-style-type: none }
+
+/** Heading element font sizes **/
+h1 { font-size: var(--fs-h1) }
+h2 { font-size: var(--fs-h2) }
+h3 { font-size: var(--fs-h3) }
+h4 { font-size: var(--fs-h4) }
+h5 { font-size: var(--fs-h5) }
+
+/** Disable anchor tag's decoration and add hover transitions **/
+a {
+    text-decoration: none;
+    transition: var(--tr-hover);
 }
+
+a:hover { color: var(--fg-tertiary) }
+
+/* Navbar and footer styling */
+/** In principle navbar and footer have basically the same styles 
+    with some alignment related differences. I.e. footer is sticky while navbar is not.
+    Also footer aligns it's children in the center while navbar doesn't.
+ **/
+ nav, footer {
+    padding: 5px 1rem;
+    display: flex;
+    align-items: center;
+    background-color: var(--bg-secondary);
+}
+
 </style>
