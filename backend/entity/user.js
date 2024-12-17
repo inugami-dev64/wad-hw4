592f8916c52d4964c2a1cd9a7b2489c270cdd91d
inugami-dev64 <karl@karlott.net>
2024-12-17 19:18:30 +0200

    Create user entity

---
+++ backend/entity/user.js
@@ -0,0 +1,55 @@
+const { ERROR_CODES } = require('../error')
+const { pool } = require('../database')
+
+class User {
+    constructor(id=null, email=null, password=null, createdAt=null) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.createdAt = createdAt;
+    }
+
+    static async getById(id) {
+        const user = await pool.query(
+            "SELECT * FROM users WHERE id = $1",
+            [id]
+        )
+        if (user.rows.length == 0)
+            throw ERROR_CODES.NotFound;
+        return new User(user.rows[0].id, user.rows[0].email, user.rows[0].created_at);
+    }
+
+    static async getByEmail(email) {
+        const user = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+
+        if (user.rows.length == 0)
+            throw ERROR_CODES.InvalidEmail;
+        return new User(user.rows[0].id, user.rows[0].email, user.rows[0].created_at);
+    }
+
+    static async isEmailInUse(email) {
+        const user = await pool.query(
+            "SELECT 1 FROM users WHERE email = $1",
+            [email]
+        );
+
+        return user.rows.length != 0;
+    }
+
+    static async createUser(user) {
+        const newUser = await pool.query(
+            // we don't want to return the password hash
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING (id,email,created_at)",
+            [post.email, post.password]
+        );
+
+        if (newUser.rows.length == 0)
+            throw new ERROR_CODES.EmailAlreadyExists;
+        return new User(newUser.rows[0].id, newUser.rows[0].email, null, newUser.rows[0].created_at);
+    }
+}
+
+module.exports = { User }
