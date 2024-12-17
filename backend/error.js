0c286a5239d30db58d9d38bd6f2e2fa9b17645e3
inugami-dev64 <karl@karlott.net>
2024-12-17 19:17:55 +0200

    Add new error codes

--- backend/error.js
+++ backend/error.js
@@ -11,7 +11,10 @@ const ERROR_CODES = {
     Success: [0, 'success'],
     NotFound: [1, 'not found'],
     NotAuthorized: [2, 'not authorized'],
-    PersistenceError: [3, 'persistence error']
+    PersistenceError: [3, 'persistence error'],
+    InvalidEmail: [4, 'invalid email'],
+    InvalidPassword: [5, 'invalid password'],
+    EmailAlreadyExists: [6, 'email is already in use']
 }
 
 module.exports = { HttpError, ERROR_CODES };
